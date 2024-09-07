00e6fcabb9fa6294a0d69ed7055d12616a7cc85e
alireza <afshar2451382@gmail.com>
2024-09-07 13:09:35 +0330

    e-topbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@
 import React from "react";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   let router = useRoutes(routes);
-  return <div>{router}</div>;
+  return (
+    <>
+      <Topbar />
+      <div>{router}</div>
+    </>
+  );
 }
 
 export default App;
