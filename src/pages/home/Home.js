acc7e4a8c0473604fe9cb80b6f033e2407491993
alireza <afshar2451382@gmail.com>
2024-09-08 16:12:42 +0330

    e-nemodar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Features from "./../../components/features/Feature";
+import Chart from "../../components/Chart/Chart";
+import { xAxisData } from "../../datas";
 
 export default function Home() {
-  return <Features />;
+  return (
+    <div className="home">
+      <Features />
+      <Chart title="month Sale" grid data={xAxisData} dataKey="Sale" />
+    </div>
+  );
 }
